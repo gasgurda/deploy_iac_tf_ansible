a5f49431c5b4db8e8be1fba2f8288c342debe2d4
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2021-10-24 22:32:52 +0300

    Added deploy jenkins instances tf files.

--- security_groups.tf
+++ security_groups.tf
@@ -43,10 +43,10 @@ resource "aws_security_group" "jenkins-sg" {
     cidr_blocks = [var.external_ip]
   }
   ingress {
-    description = "Allow anyone on port 8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    description     = "Allow anyone on port 8080"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb-sg.id]
   }
   ingress {
