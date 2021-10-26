e60b1661dc26ac04b5909d3e191329055d7d3e7d
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2021-10-26 21:00:11 +0300

    Fix syntax mistake.

--- alb.tf
+++ alb.tf
@@ -1,8 +1,8 @@
-resource "aws_alb" "application-lb" {
+resource "aws_lb" "application-lb" {
   provider = "aws.region-master"
   name = "jenkins-lb"
   internal = false
-  load_balancer_type = "applicaion"
+  load_balancer_type = "application"
   security_groups = [aws_security_group.lb-sg.id]
   subnets = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
   tags = {
